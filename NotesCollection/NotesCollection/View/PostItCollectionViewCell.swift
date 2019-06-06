0c49e2389e3814e0ce2a944c6b44e837a3bdb1bb
Pedro <peslopes@inf.ufrgs.br>
2019-06-06 17:19:57 -0300

    Create post it collection view cell file

---
+++ NotesCollection/NotesCollection/View/PostItCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostItCollectionViewCell.swift
+//  NotesCollection
+//
+//  Created by Pedro Enrique Sobrosa Lopes on 06/06/19.
+//  Copyright © 2019 Pedro. All rights reserved.
+//
+
+import UIKit
+
+class PostItCollectionViewCell: UICollectionViewCell {
+    
+}
