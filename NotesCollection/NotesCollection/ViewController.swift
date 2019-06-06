6fbe602c8aae6f5152290c50728a4a91d529e55c
Pedro <peslopes@inf.ufrgs.br>
2019-06-06 16:18:26 -0300

    First commit

---
+++ NotesCollection/NotesCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesCollection
+//
+//  Created by Pedro Enrique Sobrosa Lopes on 06/06/19.
+//  Copyright © 2019 Pedro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
